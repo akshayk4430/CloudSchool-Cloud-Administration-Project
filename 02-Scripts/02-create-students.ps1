28e8fe4dc79ed77b2811702f261f0c2d50602f04
akshayk4430 <112807357+akshayk4430@users.noreply.github.com>
2026-04-08 08:39:32 +0400

    Update 02-create-students.ps1

--- 02-Scripts/02-create-students.ps1
+++ 02-Scripts/02-create-students.ps1
@@ -1,1 +1,24 @@
+# 02-create-students.ps1
 
+$students = Import-Csv ".\data\students.csv"
+
+foreach ($student in $students) {
+
+    $passwordProfile = @{
+        Password = "P@ssw0rd123!"
+        ForceChangePasswordNextSignIn = $true
+    }
+
+    New-MgUser -DisplayName $student.DisplayName `
+        -UserPrincipalName $student.UPN `
+        -AccountEnabled $true `
+        -MailNickname ($student.UPN.Split("@")[0]) `
+        -PasswordProfile $passwordProfile `
+        -AdditionalProperties @{
+            "extensionAttribute1" = $student.Grade
+            "extensionAttribute2" = $student.Division
+            "employeeType" = "Student"
+        }
+
+    Write-Host "Created student: $($student.DisplayName)"
+}
