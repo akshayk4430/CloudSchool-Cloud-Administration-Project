8eaaa7e0ff10b98a29fffd22ea1a8ee28557705c
akshayk4430 <112807357+akshayk4430@users.noreply.github.com>
2026-04-08 08:43:15 +0400

    Update 03-create-staff.ps1

--- 02-Scripts/03-create-staff.ps1
+++ 02-Scripts/03-create-staff.ps1
@@ -1,1 +1,23 @@
+# 03-create-staff.ps1
 
+$staffs = Import-Csv ".\data\staff.csv"
+
+foreach ($staff in $staffs) {
+
+    $passwordProfile = @{
+        Password = "P@ssw0rd123!"
+        ForceChangePasswordNextSignIn = $true
+    }
+
+    New-MgUser -DisplayName $staff.DisplayName `
+        -UserPrincipalName $staff.UPN `
+        -AccountEnabled $true `
+        -MailNickname ($staff.UPN.Split("@")[0]) `
+        -Department $staff.Department `
+        -PasswordProfile $passwordProfile `
+        -AdditionalProperties @{
+            "employeeType" = "Staff"
+        }
+
+    Write-Host "Created staff: $($staff.DisplayName)"
+}
