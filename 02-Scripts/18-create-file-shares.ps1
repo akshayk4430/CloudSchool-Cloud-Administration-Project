f975b7faa264af77bc5a6c6f2826886898dd3fe8
Akshay Kattoth <akshaykattoth@gmail.com>
2026-08-15 13:13:31 +0400

    T12: file share script skeleton with idempotency check

--- 02-Scripts/18-create-file-shares.ps1
+++ 02-Scripts/18-create-file-shares.ps1
@@ -1,23 +1,33 @@
 $shares = Import-Csv -Path ".\03-CSV-Templates\file-shares.csv"
-
-$existingShares = @('staff-shared', 'school-templates')
+$rg = "RG-Storage-Prod"
+$sa = "stcloudschoolprod001"
+$existingSharesObjects = Get-AzRmStorageShare -ResourceGroupName $rg -StorageAccountName $sa -ErrorAction SilentlyContinue
+$existingShares = $existingSharesObjects.Name
 $results = @()
 foreach ($share in $shares) {
     if ($existingShares -contains $share.ShareName) {
         Write-Host "$($share.ShareName) is already available"
+        $shareObject = $existingSharesObjects | Where-Object {$_.Name -eq $share.ShareName}
         $status = "Exists"
     }
     else {
         Write-Host "$($share.ShareName) is not available"
-        $status = "WillCreate"       
+        $shareObject = New-AzRmStorageShare `
+                        -ResourceGroupName $rg `
+                        -StorageAccountName $sa `
+                        -Name $share.ShareName `
+                        -QuotaGiB ([int]$share.QuotaGiB) `
+                        -AccessTier $share.AccessTier
+        $status = "Created"       
     }
     $results += [PSCustomObject]@{
         ShareName  = $share.ShareName
-        AccessTier = $share.AccessTier
+        AccessTier = $shareObject.AccessTier
         Status     = $status
+        QuotaGiB   = $shareObject.QuotaGiB
         Timestamp  = Get-Date
     }
 }
 
 $results | Format-Table
-$results | Export-Csv -Path ".05-Outputs\t12-file-shares-dryrun.csv"
+$results | Export-Csv -Path ".\05-Outputs\t12-file-shares.csv"
